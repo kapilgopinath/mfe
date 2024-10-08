cbce4642ef751ecd86d8e2a8b7a5b3be73bce9c2
Gopinath <b113260@me-kv7p7ypvjq.na.global.prv>
2024-10-08 14:24:24 -0500

    First commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+    return (
+        <>
+            <h1>
+                Hi , there!!
+            </h1>
+            <hr/>
+            <MarketingApp />
+        </>
+
+    )
+}
